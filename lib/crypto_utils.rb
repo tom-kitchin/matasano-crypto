ab04386849b610b91cc21508edd4f5f56abd6499
Tom Kitchin <thomas.kitchin@yoomee.com>
2015-10-19 14:45:58 +0100

    Cleanup, remove stuff which is just misleading

--- lib/crypto_utils.rb
+++ lib/crypto_utils.rb
@@ -1,26 +1,22 @@
 module CryptoUtils
   module_function
 
+  def hex_to_base64(hex)
+    bin_array_to_base64(hex_string_to_bin_array(hex))
+  end
+
   def hex_xor(hex1, hex2)
     bin1 = hex_string_to_int(hex1)
     bin2 = hex_string_to_int(hex2)
     int_to_hex_string(bin1 ^ bin2)
   end
 
-  def hex_to_base64(hex)
-    bin_to_base64(hex_string_to_bin_array(hex))
-  end
-
-  def bin_to_base64(hex)
-    [hex].pack('m0')
-  end
-
-  def bin_array_to_hex_string(bin)
-    bin.unpack('H*').first
-  end
-
   def hex_string_to_bin_array(hex)
     hex.scan(/../).map(&:hex).pack('c*')
+  end
+
+  def bin_array_to_base64(hex)
+    [hex].pack('m0')
   end
 
   def int_to_hex_string(int)
--- spec/set_1/challenge_1_spec.rb
+++ spec/set_1/challenge_1_spec.rb
@@ -6,12 +6,4 @@ RSpec.describe :Challenge1 do
     base64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
     expect(CryptoUtils.hex_to_base64(hex)).to eq(base64)
   end
-
-  it 'converts binary input to base64' do
-    bin = CryptoUtils.hex_string_to_bin_array(
-      '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    )
-    base64 = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-    expect(CryptoUtils.bin_to_base64(bin)).to eq(base64)
-  end
 end
